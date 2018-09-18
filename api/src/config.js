374f4dab4c4f7d04bb41871183072fd383524391
Tobias Neil <brortobiasneil@gmail.com>
2018-09-18 14:16:55 +0200

    api: Users

--- api/src/config.js
+++ api/src/config.js
@@ -17,13 +17,23 @@ module.exports = {
     UB9BP16UX: "TJ",
     U5AUHCR2B: "Tobias",
     UB292NRFH: "James",
+    U7XLLUX1U: "Kristina",
+    U53DU5SD7: "Valerie",
     U0362PZ56: "Strömmis"
   },
   rails: {
     users: {
-      U025LF8BP: "U025LF8BP",
-      U15G19DGE: "U15G19DGE",
-      U3V0H9NLA: "U3V0H9NLA"
+      "5f822278-74b2-478d-97fe-826029c38382": "U3V0H9NLA",
+      "07555e08-4f7b-4b50-81d6-ad6c2f4906ba": "U3V0H9NLA",
+      "db717848-90b4-47ab-83eb-de557d26d807": "U3V0H9NLA",
+      "a87ff679-a2f3-471d-9181-a67b7542122c": "U025LLUMV",
+      "def7325c-5926-4b40-ab14-6261ba54dcb0": "U025LLUMV",
+      "eb106ac9-f214-4a94-8c68-4b373e5b3037": "U5AUHCR2B",
+      "db979243-b9b1-4a97-b32a-3372218c178d": "U7XLLUX1U",
+      "fd15d5f6-a47b-4aea-aa05-123119a5d5d4": "U78656JG0",
+      "35f73335-2072-499e-a612-d2ea0b46cdb1": "UB9BP16UX",
+      "563cb437-78c0-4837-bf13-3b7861e35dab": "U15G19DGE",
+      "9f9bd1e8-d318-452a-b864-5885039c1420": "U53DU5SD7",
     }
   }
 };
